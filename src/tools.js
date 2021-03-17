91e6005ef44e21f113420085d674f750601bec07
Noah <noahrules96950@gmail.com>
2021-03-18 03:25:02 +1000

    Discover Saipan!

---
+++ src/tools.js
@@ -0,0 +1,9 @@
+// maintain the header across the entire screen
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
